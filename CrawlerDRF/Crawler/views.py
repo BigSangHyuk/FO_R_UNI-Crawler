7c38c7497ddd9c36edd7f46dbc2eaf1418b04b84
KimDongHu <ehdgn1022@naver.com>
2024-01-26 21:37:07 +0900

    ✨ views.py 추가

--- CrawlerDRF/Crawler/views.py
+++ CrawlerDRF/Crawler/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def Showdata(request):
+    data = [
+        {'category_id':'', 'title':'1', 'content':'', 'img_url':'', 'posted_at':'', 'deadline':''},
+        {'category_id':'', 'title':'2', 'content':'', 'img_url':'', 'posted_at':'', 'deadline':''},
+        {'category_id':'', 'title':'3', 'content':'', 'img_url':'', 'posted_at':'', 'deadline':''},
+    ]
+    serializer = data
+    return Response(serializer)
