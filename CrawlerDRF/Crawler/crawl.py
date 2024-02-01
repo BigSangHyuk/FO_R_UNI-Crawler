552ddfb1dd9bb45b6ac7493ae47c223e6a7fac97
KimDongHu <ehdgn1022@naver.com>
2024-02-01 17:41:38 +0900

    ♻️ 비동기처리 & 포맷팅

--- CrawlerDRF/Crawler/crawl.py
+++ CrawlerDRF/Crawler/crawl.py
@@ -3,60 +3,70 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 from .gpt import get_ai_response
 import re
+import aiohttp
+import asyncio
 
-def getHtml(url):
-    res = requests.get(url)
-    
-    html = res.text
-    
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    return soup
 
-def CrawlNotice(department):
-    data = [CrawlDepartment(dm) for dm in department]
-    mydata = sum(data, [])
-    return mydata
+async def fetch_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.text()
 
-def CrawlDepartment(dm):
-    
+
+async def CrawlDepartment(dm):
     data = []
     code = dm[0]
     number = dm[1]
 
-    list_url = f'https://inu.ac.kr/bbs/{code}/{number}/artclList.do'
+    list_url = f"https://inu.ac.kr/bbs/{code}/{number}/artclList.do"
+    html = await fetch_html(list_url)
 
-    soup = getHtml(list_url)
+    soup = BeautifulSoup(html, "html.parser")
 
-    notice_list = soup.select('body table tbody > tr:not(.notice)')
+    notice_list = soup.select("body table tbody > tr:not(.notice)")
     today = datetime.now().strftime("%Y.%m.%d")
-    # today = '2024.01.29'
-    
-    for notice in notice_list:
-        posted_at = notice.select_one('.td-date').text.strip() if notice.select_one('.td-date') else ''
-        if today == posted_at:
-            
-            notice_url = 'https://inu.ac.kr' + notice.select_one('.td-subject > a').get('href')
+    today = "2024.01.29"
 
-            title = notice.select_one('.td-subject strong').text
-            
-            # writer = notice.select_one('.td-write').text.strip()
-            
-            soup = getHtml(notice_url)
-            
-            content = soup.select_one('.view-con')
-            
-            content_text = re.sub(r'(\n)+', '\n', re.sub(r'\t|\xa0', '', content.text))
-            
-            if img := content.select('p img'):
-                img_url = [i.get('src') for i in img]
-            
+    for notice in notice_list:
+        posted_at = (
+            notice.select_one(".td-date").text.strip()
+            if notice.select_one(".td-date")
+            else ""
+        )
+        if today == posted_at:
+            notice_url = "https://inu.ac.kr" + notice.select_one(".td-subject > a").get(
+                "href"
+            )
+            title = notice.select_one(".td-subject strong").text
+
+            html = await fetch_html(notice_url)
+            content = BeautifulSoup(html, "html.parser").select_one(".view-con")
+            content_text = re.sub(r"(\n)+", "\n", re.sub(r"\t|\xa0", "", content.text))
+
+            if img := content.select("p img"):
+                img_url = [i.get("src") for i in img]
             else:
-                img_url = ''
-            
+                img_url = ""
+
             # deadline = get_ai_response(re.sub(r'\n', '', content_text))
-            deadline = ''
-            
-            data.append({'category_id':number, 'title':title, 'content':content_text, 'img_url':img_url, 'posted_at':posted_at, 'deadline':deadline, 'notice_url':notice_url})
-        
+            deadline = ""
+
+            data.append(
+                {
+                    "category_id": number,
+                    "title": title,
+                    "content": content_text,
+                    "img_url": img_url,
+                    "posted_at": posted_at,
+                    "deadline": deadline,
+                    "notice_url": notice_url,
+                }
+            )
+
     return data
+
+
+async def CrawlNotice(department):
+    tasks = [CrawlDepartment(dm) for dm in department]
+    results = await asyncio.gather(*tasks)
+    return sum(results, [])
--- CrawlerDRF/Crawler/views.py
+++ CrawlerDRF/Crawler/views.py
@@ -2,12 +2,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .crawl import CrawlNotice
 from .department import get_department
+import asyncio
 
 department = get_department()
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def Showdata(request):
-    
-    data = CrawlNotice(department)
-    
+
+    data = asyncio.run(CrawlNotice(department))
+
     return Response(data)
